c1ddd591432ebf029dd6de299af3f8853b4834b2
14vosx <14vosx@gmail.com>
2026-04-27 14:15:00 -0300

    feat(portal): adicionar shell Angular e tokens base

---
+++ frontend/angular/src/app/core/layout/portal-footer/portal-footer.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portal-footer',
+  templateUrl: './portal-footer.html',
+  styleUrl: './portal-footer.css',
+})
+export class PortalFooter {}
